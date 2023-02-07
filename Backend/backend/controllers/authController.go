3029aa89ed7f879a029f209ccd0a4ce2352b9e74
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-07 14:26:31 -0500

    Implemented jwt for creation of tokens to eventually use for cookies

--- Backend/backend/controllers/authController.go
+++ Backend/backend/controllers/authController.go
@@ -4,7 +4,10 @@ import (
 	"Ga1ors/database"
 	"Ga1ors/models"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
+	"strconv"
+	"time"
 )
 
 func Register(c *fiber.Ctx) error {
@@ -60,5 +63,16 @@ func Login(c *fiber.Ctx) error {
 		})
 	}
 
-	return c.JSON(user)
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer:    strconv.Itoa(int(user.Id)),
+		ExpiresAt: &jwt.NumericDate{time.Now().Add(time.Hour * 24)},
+	})
+
+	token, err := claims.SignedString([]byte("secret"))
+
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(token)
 }
