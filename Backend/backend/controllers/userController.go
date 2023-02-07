2c254c05ead02ab74d81febd8839f56438f23b0a
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-07 15:15:16 -0500

    Implemented user controller to get all users and create users using get/post requests respectively

---
+++ Backend/backend/controllers/userController.go
@@ -0,0 +1,32 @@
+package controllers
+
+import (
+	"Ga1ors/database"
+	"Ga1ors/models"
+	"github.com/gofiber/fiber/v2"
+	"golang.org/x/crypto/bcrypt"
+)
+
+func AllUsers(c *fiber.Ctx) error {
+	var users []models.User
+
+	database.DB.Find(&users)
+
+	return c.JSON(users)
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	var user models.User
+
+	if err := c.BodyParser(&user); err != nil {
+		return err
+	}
+
+	password, _ := bcrypt.GenerateFromPassword([]byte("1234"), 14)
+
+	user.Password = password
+
+	database.DB.Create(&user)
+
+	return c.JSON(user)
+}
