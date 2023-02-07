1166263a71db9828e5be62bbc6666079c4c9f78c
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-07 14:44:06 -0500

    Implemented login in order to retrieve authenticated user

--- Backend/backend/controllers/authController.go
+++ Backend/backend/controllers/authController.go
@@ -87,3 +87,26 @@ func Login(c *fiber.Ctx) error {
 		"message": "Successful login",
 	})
 }
+
+func User(c *fiber.Ctx) error {
+	cookie := c.Cookies("jwt")
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.RegisteredClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "User unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*jwt.RegisteredClaims)
+
+	var user models.User
+
+	database.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	return c.JSON(user)
+}
