537f6883f44ce76ad729951e1f9dcb6640f1b289
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-07 14:50:27 -0500

    Json output no longer shows password

--- Backend/backend/models/user.go
+++ Backend/backend/models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	Id        uint
-	FirstName string
-	LastName  string
-	Email     string `gorm:"unique"` // email should be unique in database
-	Password  []byte
+	Id        uint   `json:"id"`
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Email     string `json:"email" gorm:"unique"` // email should be unique in database
+	Password  []byte `json:"-"`                   // password won't show in api requests
 }
