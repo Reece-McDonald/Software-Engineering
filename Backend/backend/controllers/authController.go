d000c7d1d8768c7f045158d2862403da9a71b360
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-07 14:10:43 -0500

    Implemented user login, able to check database for current users

--- Backend/backend/controllers/authController.go
+++ Backend/backend/controllers/authController.go
@@ -34,3 +34,31 @@ func Register(c *fiber.Ctx) error {
 
 	return c.JSON(user)
 }
+
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+
+	database.DB.Where("email = ?", data["email"]).First(&user) // get user from database
+
+	if user.Id == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(400)
+		return c.JSON(fiber.Map{
+			"message": "Passwords do not match",
+		})
+	}
+
+	return c.JSON(user)
+}
--- Backend/backend/go.mod
+++ Backend/backend/go.mod
@@ -4,6 +4,7 @@ go 1.20
 
 require (
 	github.com/gofiber/fiber/v2 v2.42.0
+	golang.org/x/crypto v0.5.0
 	gorm.io/driver/mysql v1.4.5
 	gorm.io/gorm v1.24.5
 )
