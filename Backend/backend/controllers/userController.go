fe7e3019cad19181e5793d61a417d9db8f0071b5
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-07 15:44:47 -0500

    Implemented way to store json user data into different pages

--- Backend/backend/controllers/userController.go
+++ Backend/backend/controllers/userController.go
@@ -4,15 +4,30 @@ import (
 	"Ga1ors/database"
 	"Ga1ors/models"
 	"github.com/gofiber/fiber/v2"
+	"math"
 	"strconv"
 )
 
 func AllUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 5
+	offset := (page - 1) * limit
+
+	var totalUsers int64
 	var users []models.User
 
-	database.DB.Find(&users)
+	database.DB.Offset(offset).Limit(limit).Find(&users)
 
-	return c.JSON(users)
+	database.DB.Model(&models.User{}).Count(&totalUsers)
+
+	return c.JSON(fiber.Map{
+		"data": users,
+		"meta": fiber.Map{
+			"total":    totalUsers,
+			"page":     page,
+			"lastPage": math.Ceil(float64(int(totalUsers) / limit)),
+		},
+	})
 }
 
 func CreateUser(c *fiber.Ctx) error {
