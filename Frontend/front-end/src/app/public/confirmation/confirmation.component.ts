0f7481c3d45b5943f9badd1e90418c3b85dc0ee7
ChristianBell002 <Christian.Bello@ufl.edu>
2023-02-26 22:35:39 -0500

    Added/edited confirm page. Need to fix and sync w/backend

--- Frontend/front-end/src/app/public/confirmation/confirmation.component.ts
+++ Frontend/front-end/src/app/public/confirmation/confirmation.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
   selector: 'app-confirmation',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./confirmation.component.css']
 })
 export class ConfirmationComponent {
+  email = '';
+  
+  constructor(
+    private router: Router,
+    private authService: AuthService) {
+  }
 
-}
+  submit(): void {
+    console.log({
+      email: this.email
+  })
+
+  this.authService.register({ // don't NEED to do this, just send it to backend.
+    email: this.email
+  })
+}}
--- Frontend/front-end/src/app/public/public.module.ts
+++ Frontend/front-end/src/app/public/public.module.ts
@@ -1,19 +1,18 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {PublicComponent} from './public.component';
+import {ConfirmationComponent } from './confirmation/confirmation.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {RouterLink, RouterOutlet} from "@angular/router";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { ConfirmationComponent } from './confirmation/confirmation.component';
-
 
 @NgModule({
   declarations: [
     PublicComponent,
+    ConfirmationComponent,
     LoginComponent,
     RegisterComponent,
-    ConfirmationComponent
   ],
   imports: [
     CommonModule,
