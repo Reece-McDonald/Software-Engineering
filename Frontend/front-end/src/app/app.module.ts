95f3fc159f1b988d40baef9f6dbc926d25ab60ae
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-02-27 22:46:43 -0500

    Added cypress testing examples and set up a few basic tests for the register component to mount the component and populate the fields. Clicking the submit button does not seem to work currently, it either creates an infinite loop or doesn't register the window alert. Also added a simple check for empty input forms and/or incorrect email.

--- Frontend/front-end/src/app/app.module.ts
+++ Frontend/front-end/src/app/app.module.ts
@@ -6,10 +6,12 @@ import {AppComponent} from './app.component';
 import {SecureModule} from "./secure/secure.module";
 import {PublicModule} from "./public/public.module";
 import {HttpClientModule} from "@angular/common/http";
+import { StepperComponent } from './testing/cypress-example/stepper/stepper.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    StepperComponent,
   ],
   imports: [
     BrowserModule,
