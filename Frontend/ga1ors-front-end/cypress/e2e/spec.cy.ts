4d1cce0a1c39e6369c1082eedd9a807ec3070363
ChristianBell002 <Christian.Bello@ufl.edu>
2023-03-29 17:51:45 -0400

    Created a simple e2e test to test navigation

---
+++ Frontend/ga1ors-front-end/cypress/e2e/spec.cy.ts
@@ -0,0 +1,28 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:4200/login')
+    cy.wait(900)
+    cy.contains('Register').click()
+    cy.url().should('include', '/register')
+    cy.wait(2000)
+
+    cy.get('[name=firstName]').type('Christian')
+    cy.get('[name=lastName]').type('Bello')
+    cy.get('[name=email]').type('test@test.com')
+    cy.get('[name=password]').type('test')
+    cy.get('[name=passwordConfirm]').type('test')
+    cy.wait(1000)
+  })
+
+  it('navigates to another link', () =>{
+    cy.visit('http://localhost:4200/chat/messages')  
+    cy.wait(1000)
+    cy.get('[name=message]').type('Hello. This is a test message.')
+    cy.wait(1000)
+    cy.contains('Sign Out').click()
+    cy.url().should('include', '/login')
+    cy.wait(500)
+  
+
+  })
+})
